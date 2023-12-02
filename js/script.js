9f1e273b22292c0bec7c88546012e27d5c7db59d
Paulo Sergio <pscosta2008@gmail.com>
2023-12-02 10:15:18 -0300

    feat: Adicionando projeto local ao repositório

---
+++ js/script.js
@@ -0,0 +1,191 @@
+let totalCarregar = 12
+let totalPokemons = 1000
+
+function openDropDown(){
+
+    const DropDownContainer = document.getElementsByClassName("dropDown-menu")[0]
+    const DropDownArrow = document.getElementById("arrow-dropDown")
+    if(DropDownArrow.className == "closed"){
+        // arrow animation
+        DropDownArrow.classList.remove("closed")
+        DropDownArrow.classList.add("opened")
+
+        //open DropDown
+
+        DropDownContainer.style.display = "block"
+
+        return
+    }
+    // arrow animation
+    DropDownArrow.classList.remove("opened")
+    DropDownArrow.classList.add("closed")
+    
+    //open DropDown
+
+    DropDownContainer.style.display = "none"
+
+    return
+
+}
+
+const opOrganization = document.getElementsByClassName("opOrganization")
+
+for(let i = 0; i < 4; i++){
+    opOrganization[i].addEventListener('click', changeOrganizationPokemons)
+}
+
+function changeOrganizationPokemons(event){
+    // change name
+    document.getElementsByClassName("dropDown-text-content")[0].innerHTML = event.target.textContent
+
+    // organization logic
+    deletePokemons()
+    if(event.target == opOrganization[0]){
+        ordemCrescente(totalCarregar)
+    }else if(event.target == opOrganization[1]){
+        ordemDecrescente(totalPokemons, totalCarregar)
+    }else if(event.target == opOrganization[2]){
+
+    }else if(event.target == opOrganization[3]){
+
+    }
+}
+
+const POKEMON_BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
+
+async function ordemCrescente(j){
+    for(let i = 1; i <= j; i++){
+        const result = await getAPI(i)
+    }
+}
+
+async function ordemDecrescente(){
+    for(let i = totalPokemons; i > totalPokemons-totalCarregar; i--){
+        // deletePokemons(i)
+        const result = await getAPI(i)
+    }
+}
+
+async function getAPI(i){
+    await fetch(POKEMON_BASE_URL + `${i}`).then(async response =>{
+        return response.json().then(pokemon =>{
+
+            const name = pokemon.name
+            const id = pokemon.id
+            const img = pokemon.sprites.other["official-artwork"].front_default
+            
+            let type1 = pokemon.types[0].type.name
+            
+            let type2 = undefined
+            
+            if(pokemon.types[1] != undefined){
+                type2 = pokemon.types[1].type.name
+            }
+
+            showPokemons(name, id, img, type1, type2)
+        })
+    })
+}
+
+function deletePokemons(){
+    let Pai = document.getElementsByClassName("pokemons-results-container")[0]
+
+    while(Pai.firstChild){
+        Pai.removeChild(Pai.firstChild)
+    }
+}
+
+function showPokemons(name, id, img, type1, type2){
+
+    let DivContainer = document.createElement("div")
+    DivContainer.classList.add("pokemon-card")
+    DivContainer.classList.add("col-lg-3")
+    DivContainer.classList.add("mb-3")
+    DivContainer.classList.add("col-sm-6")
+    DivContainer.classList.add("col-md-4")
+    document.getElementsByClassName("pokemons-results-container")[0].appendChild(DivContainer)
+    
+    // 
+    
+    let DivImgContainer = document.createElement("div")
+    DivImgContainer.classList.add("img-pokemon")
+    DivContainer.appendChild(DivImgContainer)
+
+    let ImgPokemon = document.createElement("img")
+    ImgPokemon.src = img
+    ImgPokemon.classList.add("img-fluid")
+    DivImgContainer.appendChild(ImgPokemon)
+
+    // 
+
+    let DivDescContainer = document.createElement("div")
+    DivDescContainer.classList.add("desc-pokemon")
+    DivContainer.appendChild(DivDescContainer)
+
+
+    let idPokemon = document.createElement("p")
+    idPokemon.classList.add("id-pokemon")
+    idPokemon.innerHTML = "Nº " + id
+    DivDescContainer.appendChild(idPokemon)
+
+    let namePokemon = document.createElement("p")
+    namePokemon.classList.add("name-pokemon")
+    namePokemon.innerHTML = name
+    DivDescContainer.appendChild(namePokemon)
+
+    let DivTypesContainer = document.createElement("div")
+    DivTypesContainer.classList.add("pokemon-type")
+    DivTypesContainer.classList.add("mt-2")
+    DivTypesContainer.classList.add("row")
+    DivTypesContainer.classList.add("d-flex")
+    DivTypesContainer.classList.add("container")
+    DivDescContainer.appendChild(DivTypesContainer)
+
+    let type1Container = document.createElement("div")
+    type1Container.classList.add(`${type1}-type`)
+    type1Container.classList.add(`col-xl-6`)
+    type1Container.classList.add(`col-sm-12`)
+    type1Container.classList.add(`col-6`)
+    DivTypesContainer.appendChild(type1Container)
+
+    let type1Pokemon = document.createElement("p")
+    type1Pokemon.innerHTML = type1
+    type1Pokemon.classList.add("type-text")
+    type1Pokemon.classList.add("text-center")
+    type1Container.appendChild(type1Pokemon)
+
+    if(type2 != undefined){
+        let type2Container = document.createElement("div")
+        type2Container.classList.add(`${type2}-type`)
+        type2Container.classList.add(`col`)
+        DivTypesContainer.appendChild(type2Container)
+
+        let type2Pokemon = document.createElement("p")
+        type2Pokemon.classList.add("type-text")
+        type2Pokemon.classList.add("text-center")
+        type2Pokemon.innerHTML = type2
+        type2Container.appendChild(type2Pokemon)
+    }
+
+}
+
+async function searchPokemon(){
+    deletePokemons()
+    let input = document.getElementById("search-bar-input")
+    if(!isNaN(input.value)){
+        const result = await getAPI(input.value) 
+        return
+    }
+
+    // const resultSearch = listaPokemonsName.filter((name) =>{
+    //     name.toLowerCase().includes(input.value.toLowerCase())
+    // })
+    // console.log(listaPokemonsName)
+    // for(let i = 0; i < resultSearch; i++){
+    //     // const result = await getAPI(i) 
+    // }
+
+
+}
+
+ordemCrescente(totalCarregar)
